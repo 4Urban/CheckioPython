4dab20d43ca08a77e0903d938da0c65e0c71492d
서성주 <uplife2100@yonsei.ac.kr>
2020-08-03 21:33:17 +0900

    acceptable_password_ii

--- Electronic Station/acceptable_password_ii.py
+++ Electronic Station/acceptable_password_ii.py
@@ -11,30 +11,15 @@
 # 
 # END_DESC
 
-# Taken from mission Acceptable Password I
+import re
 
 def is_acceptable_password(password: str) -> bool:
     # your code here
-    if len(password) > 6:
+    regex = re.compile('\d')
+    if len(password) > 6 and regex.search(password):
         return True
     else:
         return False
-
-
-if __name__ == '__main__':
-    print("Example:")
-    print(is_acceptable_password('short'))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert is_acceptable_password('short') == False
-    assert is_acceptable_password('muchlonger') == True
-    assert is_acceptable_password('ashort') == False
-    print("Coding complete? Click 'Check' to earn cool rewards!")
-
-
-def is_acceptable_password(password: str) -> bool:
-    # your code here
-    return False
 
 
 if __name__ == '__main__':
